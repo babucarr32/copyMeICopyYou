dd25534dff937f80bc394749f13541d7b27bf37c
babucarr32 <baboucarry@gmail.com>
2022-10-29 17:25:19 +0000

    adding the pyperclip lib

--- client1.py
+++ client1.py
@@ -1,4 +1,5 @@
 import socket
+import pyperclip
 
 def SimpleTCPServer():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,6 +10,7 @@ def SimpleTCPServer():
     while True:
         msg = "Hello World"
         s.send(msg.encode('utf-8'))
-        msg_recv = s.recv(1000000)
+        msg_recv = msg_recv = s.recv(1000000)
+        pyperclip.copy(msg_recv.decode('utf-8'))
         print(msg_recv.decode("utf-8"))
 SimpleTCPServer()
