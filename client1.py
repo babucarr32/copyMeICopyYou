a571ff7e6aa6fc42f46009b503130b9a6dca1866
babucarr32 <baboucarry@gmai.com>
2023-09-03 12:51:22 +0000

    Debugging

--- client1.py
+++ client1.py
@@ -2,10 +2,11 @@ import socket
 import pyperclip
 import sys
 import select
+import time
 
 def SimpleTCPServer():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host =  "10.1.1.135"
+    host = "10.1.1.135"
     port = 55555
     s.connect((host, port))
 
@@ -14,51 +15,53 @@ def SimpleTCPServer():
     msg_recv = msg_recv = s.recv(1000000)
     pyperCopy = pyperclip.copy(msg_recv.decode('utf-8'))
     newPyper = pyperclip.paste()
+    pasteDict = {"key": newPyper}
+    timeout = 2
 
     while True:
         # check for clipboard
-        # while True:
-        #     pyperPaste = pyperclip.paste() # Determine cur and prev paste
+        print("Checking...")
+        while True:
+            pyperPaste = pyperclip.paste() # Determine cur and prev paste
 
-        #     if pyperPaste == newPyper: # if pastes are the same pass
-        #         pass
-        #     else:
-        #         pyperPaste = pyperclip.paste() # paste
-        #         s.send(str(pyperPaste).encode('utf-8')) # send copied msg
-        #         msg_recv = s.recv(1000000)
-        #         pyperclip.copy(msg_recv.decode('utf-8'))
-        #         pyperPaste = pyperclip.paste() # paste
-        #         newPyper = pyperPaste # redefine newPyper
-        #         break
-        # print("breaked")
-        # print(pyperPaste)
-        sockets_list = [sys.stdin, s]
-    
-        """ There are two possible input situations. Either the
-        user wants to give manual input to send to other people,
-        or the server is sending a message to be printed on the
-        screen. Select returns from sockets_list, the stream that
-        is reader for input. So for example, if the server wants
-        to send a message, then the if condition will hold true
-        below.If the user wants to send a message, the else
-        condition will evaluate as true"""
-        read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
-    
-        for socks in read_sockets:
-            if socks == s:
-                msg = socks.recv(2048)
-                pyperclip.copy(msg.decode('utf-8'))
-                print (msg)
+            if pyperPaste == pasteDict['key']: # if pastes are the same pass
+                s.settimeout(2)  # Set a timeout of 2 seconds for receiving data
+
+                while True:
+                    try:
+                        # Receive data from the server
+                        msg_recv = s.recv(1000000)
+
+                        if not msg_recv:
+                            # No data received, stop listening
+                            print("No data received, stop listening")
+                            break
+
+                        # Process the received message
+                        print("Received message:", msg_recv.decode('utf-8'))
+                        pyperclip.copy(msg_recv.decode('utf-8'))
+
+                    except socket.timeout:
+                        # Timeout occurred, stop listening
+                        print("Timeout occured")
+                        break
+
+
+                print("passing.....", pasteDict)
+                pass
             else:
-                pyperPaste = pyperclip.paste() # Determine cur and prev paste
-
-                # if pyperPaste == newPyper: # if pastes are the same pass
-                #     pass
-                # else:
-                # message = sys.stdin.readline()
-                pasteMsg = pyperclip.paste()
-                s.send(pasteMsg.encode('utf-8'))
-                # sys.stdout.write("<You>")
-                # sys.stdout.write(message)
-                # sys.stdout.flush()
+                try:
+                    print("Different.....")
+                    pyperPaste = pyperclip.paste() # paste
+                    pasteDict['key'] = pyperPaste
+                    s.send(str(pyperPaste).encode('utf-8')) # send copied msg
+                    msg_recv = s.recv(1000000)
+                    pyperclip.copy(msg_recv.decode('utf-8'))
+                    pyperPaste = pyperclip.paste() # paste
+                    newPyper = pyperPaste # redefine newPyper
+                except Exception as e:
+                    continue
+        print("breaked")
+        print(pyperPaste)
+    
 SimpleTCPServer()
--- server.py
+++ server.py
@@ -1,10 +1,63 @@
-from ast import While
+# import socket
+# import pyperclip
+
+# def SimpleTCPServer():
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     host =  ""
+#     port = 55555
+#     s.bind((host, port))
+#     s.listen()
+#     clients = []
+
+#     print("Listening...")
+#     while True:
+#         c, addr =  s.accept()
+#         clients.append(c)
+#         print(f"Got connection from {addr}")
+
+#         if len(clients) == 2:
+#             while True:
+#                 print("Waiting to receive.....")
+#                 msg_recv = c.recv(1000000)
+#                 msg  = msg_recv.decode('utf-8')
+                
+#                 for client in clients:
+#                     client.send(msg.encode('utf-8'))
+#                     print(msg_recv)
+#         else:
+#             pass
+# SimpleTCPServer()
+
+
+
+
 import socket
-import pyperclip
+import threading
+
+def handle_client(client, clients):
+    while True:
+        try:
+            msg_recv = client.recv(1000000)
+            if not msg_recv:
+                break
+
+            msg = msg_recv.decode('utf-8')
+            print("Message Received is: ", msg)
+
+            for other_client in clients:
+                if other_client != client:
+                    other_client.send(msg.encode('utf-8'))
+        except Exception as e:
+            print(f"Error handling client: {str(e)}")
+            break
+
+    # Remove the client from the list
+    clients.remove(client)
+    client.close()
 
 def SimpleTCPServer():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host =  ""
+    host = ""
     port = 55555
     s.bind((host, port))
     s.listen()
@@ -12,23 +65,13 @@ def SimpleTCPServer():
 
     print("Listening...")
     while True:
-        # accept 
-        c, addr =  s.accept()
+        c, addr = s.accept()
         clients.append(c)
         print(f"Got connection from {addr}")
 
-        if len(clients) == 2:
-            while True:
-                # recieve message
-                msg_recv = c.recv(1000000)
-                msg  = msg_recv.decode('utf-8')
-                
-                # copy message
-                # pyperclip.copy(msg_recv.decode('utf-8'))
-                # msg = input("Enter msg: ")
-                for client in clients:
-                    client.send(msg.encode('utf-8'))
-                    print(msg_recv)
-        else:
-            pass
-SimpleTCPServer()
+        # Start a new thread to handle the client
+        client_thread = threading.Thread(target=handle_client, args=(c, clients))
+        client_thread.start()
+
+if __name__ == "__main__":
+    SimpleTCPServer()
